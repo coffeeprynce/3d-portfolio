c449af4cba7585494242ad65058b7f64b82cf774
Kyle Nguyen <kylenguyen@kyles-air.mynetworksettings.com>
2023-04-12 12:47:52 -0400

    fixed vite js, and run deployin package json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    viteCompression()
-  ],
-})
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      react(),
+      viteCompression()
+    ],
+    assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
+    base: './'
+  }
+});
